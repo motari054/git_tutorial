86bbfc87c854f68acc61cf05fcb892185b890ecb
Andrew <ogegaandrew@gmail.com>
2024-03-31 22:06:41 +0300

    final

---
+++ data_ingestion.py
@@ -0,0 +1,21 @@
+import sqlalchemy 
+import pymongo
+from sklearn.pipeline import Pipeline, make_pipeline
+
+
+def maji_ndogo_engine():    
+    engine = sqlalchemy.create_engine()
+    
+class MOdelAsessment(object):
+    
+    def __init__(self) -> None:
+        pass
+    
+    def forage_find(self, session):
+        pass
+    
+    def margianl_updater(self, time):
+        pass
+    
+    
+    
